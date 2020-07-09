a8ed25a73fa06ee80946c2e3ff646d2068dd2248
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-07-09 21:47:37 +0530

    Update Day_16.java

--- Day_16.java
+++ Day_16.java
@@ -9,5 +9,15 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
+        try
+        {
+        Integer i= Integer.parseInt(S);
+        System.out.println(i);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Bad String");
+        }
+        
     }
 }
