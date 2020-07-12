c5f2ec88c84948b0b2ec9ce5199fe3fa9b4899eb
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-07-12 22:08:06 +0530

    Update Day_17.java

--- Day_17.java
+++ Day_17.java
@@ -1,40 +1,24 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-//Write your code here
-class Calculator
-{
-    int power(int n,int p)throws Exception
-    {
-        if(n<0|| p<0)
-        {
-             throw new Exception("n and p should be non-negative");
-        }
-        else
-        {
-         return (int)Math.pow(n,p);
-        }
-    }
-}
-class Solution{
-
+public class Solution {
+    // Write your code here.
+        Stack <Character> stack=new Stack<>();
+      Queue <Character> queue=new LinkedList<>();
+      void pushCharacter(char ch)
+      {
+          stack.push(ch);
+      }
+      void enqueueCharacter(char ch)
+      {
+          queue.add(ch);   
+      }
+      char popCharacter() 
+      {
+          return stack.pop();
+      }
+      char dequeueCharacter()
+      {
+          return queue.remove();
+      }
     public static void main(String[] args) {
-    
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        while (t-- > 0) {
-        
-            int n = in.nextInt();
-            int p = in.nextInt();
-            Calculator myCalculator = new Calculator();
-            try {
-                int ans = myCalculator.power(n, p);
-                System.out.println(ans);
-            }
-            catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        in.close();
-    }
-}
