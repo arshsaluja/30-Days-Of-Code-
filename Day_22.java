fdffe46447e8da058b38b0858e712e9972291af2
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-07-13 18:34:09 +0530

    Create Day_22.java

---
+++ Day_22.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+	public static int getHeight(Node root){
+      //Write your code here
+    }
+
+	public static Node insert(Node root,int data){
