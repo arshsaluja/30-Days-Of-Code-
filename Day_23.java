8249fc50768f2f4f05ccd2175da38c4899303047
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-07-14 20:48:02 +0530

    Create Day_23.java

---
+++ Day_23.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+	static void levelOrder(Node root){
+      //Write your code here
+      
+    }
+
+	public static Node insert(Node root,int data){
