e7e01b6d96e5be44e06b662dcaad006009565bb0
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-07-15 04:32:20 +0530

    Create Day_24.java

---
+++ Day_24.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+	Node(int d){
+        data=d;
+        next=null;
+    }
+	
+}
+class Solution
+{
+
+    public static Node removeDuplicates(Node head) {
+      //Write your code here
+
+    }
+
+    public static  Node insert(Node head,int data)
