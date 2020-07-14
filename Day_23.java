fff9c443214afc846cbdc939213b884499e4bb9a
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-07-15 04:30:00 +0530

    Update Day_23.java

--- Day_23.java
+++ Day_23.java
@@ -12,7 +12,16 @@ class Solution{
 
 	static void levelOrder(Node root){
       //Write your code here
+      Queue <Node> q=new LinkedList();
+      q.add(root);
       
+      while(!q.isEmpty())
+      {
+          Node c=q.remove();
+          System.out.print(c.data+" ");
+          if(c.left!=null) q.add(c.left);
+          if(c.right!=null) q.add(c.right);
+      }
     }
 
 	public static Node insert(Node root,int data){
