3f238340a1161ce502e145dd9f7e847fbe383239
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-07-22 21:38:55 +0530

    Update Day_27.java

--- Day_27.java
+++ Day_27.java
@@ -18,26 +18,35 @@ public class Solution {
     static class TestDataEmptyArray {
         public static int[] get_array() {
             // complete this function
+         int arr[]={};
+         return arr;
         }
     }
 
     static class TestDataUniqueValues {
         public static int[] get_array() {
             // complete this function
+            int arr[]={1,2,3,4};
+            return arr;
         }
 
         public static int get_expected_result() {
             // complete this function
+            return 0;
         }
     }
 
     static class TestDataExactlyTwoDifferentMinimums {
         public static int[] get_array() {
             // complete this function
+         int arr[]={0,0,1};
+            return arr;
+
         }
 
         public static int get_expected_result() {
             // complete this function
+            return 0;
         }
     }
 
